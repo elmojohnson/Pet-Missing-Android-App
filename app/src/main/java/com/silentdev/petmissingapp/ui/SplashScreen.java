329c5339b25f108438cd65d08cdaaa036625c5c3
abc <motocapstone2021@gmail.com>
2021-06-27 16:46:10 +0800

    Added texts in strings.xml

--- app/src/main/java/com/silentdev/petmissingapp/ui/SplashScreen.java
+++ app/src/main/java/com/silentdev/petmissingapp/ui/SplashScreen.java
@@ -1,14 +1,16 @@
-package com.silentdev.petmissingapp;
+package com.silentdev.petmissingapp.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.silentdev.petmissingapp.R;
+
+public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.splash_activity);
     }
 }
